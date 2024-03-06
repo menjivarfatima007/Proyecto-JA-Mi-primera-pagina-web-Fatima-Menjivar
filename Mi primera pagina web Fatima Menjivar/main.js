3c23ada22d0e1cc011a693de2cc222d0ad59a20e
menjivarfatima007 <162407680+menjivarfatima007@users.noreply.github.com>
2024-03-05 23:07:53 -0600

    Add files via upload

---
+++ Mi primera pagina web Fatima Menjivar/main.js
@@ -0,0 +1,13 @@
+function muestra_oculta(id) {
+
+let div = document.getElementById(id);
+
+if (div.style.display == "none") {
+    div.style.display ="flex"; 
+}
+else {
+    div.style.display = "none";
+
+}
+
+}
